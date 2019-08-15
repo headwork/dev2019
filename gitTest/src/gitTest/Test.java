f6992f7f3a4b52924403db393d809a154d650174
headwork <it.sk.hwang@gmail.com>
2019-08-15 17:28:43 +0900

    fatch테스트

--- gitTest/src/gitTest/Test.java
+++ gitTest/src/gitTest/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("hello1");
+		System.out.println("hello_fatch테스트");
 		System.out.println("hello2");
 		System.out.println("hello3");
 		System.out.println("hello5");
