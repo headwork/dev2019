268e79e7c2c21518a8877683dcf536100e6ac5b3
headwork <it.sk.hwang@gmail.com>
2019-08-15 17:02:17 +0900

    1

--- gitTest/src/gitTest/Test2.java
+++ gitTest/src/gitTest/Test2.java
@@ -3,7 +3,7 @@ package gitTest;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello1");
 		System.out.println("hello2");
 	}
 }
