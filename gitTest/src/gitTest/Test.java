929b9ab70c755cc0b2cd8767e2e360c6518afad8
headwork <it.sk.hwang@gmail.com>
2020-03-30 00:18:49 +0900

    .

--- gitTest/src/gitTest/Test.java
+++ gitTest/src/gitTest/Test.java
@@ -4,7 +4,12 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("hello1");
+<<<<<<< HEAD
 		System.out.println("hello2로컬");
+=======
+		System.out.println("hello_fatch테스트");
+		System.out.println("hello2");
+>>>>>>> refs/heads/test1
 		System.out.println("hello3");
 		System.out.println("hello5");
 	}
--- gitTest/src/gitTest/Test4.java
+++ gitTest/src/gitTest/Test4.java
@@ -1,9 +1,9 @@
 package gitTest;
 
-public class Test2 {
+public class Test4 {
 
 	public static void main(String[] args) {
-		System.out.println("hello운영 업데이트");
+		System.out.println("hello");
 		System.out.println("hello2");
 	}
 }
