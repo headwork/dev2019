77c939d549aca5a8d769968259e358b10fd8655a
headwork <it.sk.hwang@gmail.com>
2021-01-23 22:21:58 +0900

    1

--- js/yt01.js
+++ js/yt01.js
@@ -1,3 +1,43 @@
-alert(1);
+//var s = document.createElement("script");
+//s.type = "text/javascript";
+//s.src = "https://headwork.github.io/dev2019/js/yt01.js";
+//document.head.appendChild(s);
+
 window._popSkip = window.open("", "test","width=100, height=100");
-window._popSkip.document.write('<sc'+'ript src="https://headwork.github.io/dev2019/js/yt.js"></sc'+'ript>');
+
+var strScrSkip = 'var temp = function(pop) {												'
++'	var obj = {                                                                         '
++'		winOpener : null,                                                               '
++'		winPop : pop                                                                    '
++'	};                                                                                  '
++'	if(pop._objSkip != null && pop._objSkip.tid != null){                               '
++'		clearInterval(pop._objSkip.tid);                                                '
++'	}                                                                                   '
++'	pop._objSkip = {                                                                    '
++'		itemSelector : ".ytp-ad-skip-button-container button.ytp-ad-skip-button"        '
++'	};                                                                                  '
++'	pop._objSkip.fnSkip = function() {                                                  '
++'		console.log("fnSkip11");                                                    '
++'		if (obj.winOpener == null)                                                      '
++'			obj.winOpener = pop.opener;                                                 '
++'		try {                                                                           '
++'			var dom = obj.winOpener.document;                                           '
++'			if (dom == null)                                                            '
++'				return;                                                                 '
++'			var v = dom.querySelectorAll(pop._objSkip.itemSelector);                    '
++'			if (v != null && v.length > 0 && v[0].offsetHeight > 0) {                   '
++'				console.log("fnSkip");                                                  '
++'				v[0].click();                                                           '
++'			}                                                                           '
++'		} catch (e) {                                                                   '
++'		}                                                                               '
++'	}                                                                                   '
++'	pop._objSkip.tid = pop.setInterval(pop._objSkip.fnSkip, 5000);                      '
++'	return obj;                                                                         '
++'};                                                                                    '
++'temp(this);                                                                           ';
+setTimeout(function() {
+	window._popSkip.document.write('<sc'+'ript>'+strScrSkip+'</sc'+'ript>');
+}, 1000);
+
+
