618f77c34421115cdf337ea24f8a38c60fdf0b51
admin_2021 <admin_2021@192.168.56.1>
2022-04-06 22:58:28 +0900

    .

--- gitTest/src/gitTest/Test.java
+++ gitTest/src/gitTest/Test.java
@@ -6,6 +6,6 @@ public class Test {
 		System.out.println("hello1");
 		System.out.println("hello3");
 		System.out.println("hello5");
-		System.out.println("hello4");
+		System.out.println("hello5");
 	}
 }
