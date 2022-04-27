db954dfd2f3cccafeaa2daacab2518370ce11e30
hsk2 <it.sk.hwang@gmail.com>
2022-04-28 00:32:19 +0900

    7

--- gitTest/src/gitTest/Test2.java
+++ gitTest/src/gitTest/Test2.java
@@ -3,10 +3,10 @@ package gitTest;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("hello운영 업데이트");
 		System.out.println("hello2");
 		System.out.println("hello4");
 		System.out.println("hello5");
 		System.out.println("hello6");
+		System.out.println("hello7");
 	}
 }
