2b5cdc0af374233b98522a5cdc2097eb16a8faa3
hsk2 <it.sk.hwang@gmail.com>
2022-04-28 00:09:15 +0900

    1

--- gitTest/src/gitTest/Test2.java
+++ gitTest/src/gitTest/Test2.java
@@ -5,6 +5,6 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("hello운영 업데이트");
 		System.out.println("hello2");
-		System.out.println("hello3");
+		System.out.println("hello4");
 	}
 }
