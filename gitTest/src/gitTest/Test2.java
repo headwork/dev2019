305239231685b5b810168c6c32d3ed1b34b40675
hsk2 <it.sk.hwang@gmail.com>
2022-05-11 06:19:23 +0900

    3,4,5 병합

--- gitTest/src/gitTest/Test2.java
+++ gitTest/src/gitTest/Test2.java
@@ -5,6 +5,9 @@ public class Test2 {
 	public static void main(String[] args) {
 	    System.out.println("hello1");
 		System.out.println("hello2");
+		System.out.println("hello3");
+		System.out.println("hello4");
+		System.out.println("hello5");
 
 	}
 }
