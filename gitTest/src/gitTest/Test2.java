c68bf197dfbd3e566811b61a54fe761dd55d3631
hsk2 <it.sk.hwang@gmail.com>
2022-05-11 06:04:44 +0900

    : New Task
    Task-Url:

--- gitTest/src/gitTest/Test2.java
+++ gitTest/src/gitTest/Test2.java
@@ -6,8 +6,6 @@ public class Test2 {
 	    System.out.println("hello1");
 		System.out.println("hello2");
 		System.out.println("hello3");
-		System.out.println("hello4"); 
-		System.out.println("hello5"); //loc 
 
 	}
 }
